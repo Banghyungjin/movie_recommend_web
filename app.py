7c11cc1e4c0ac3bb4603fcacbb0586049340816b
HyungJin_Bang <37135305+Banghyungjin@users.noreply.github.com>
2021-07-09 12:16:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,21 +5,29 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 import pandas as pd
 
 import requests
-from fetch import movie
+from fetch import movie, movie_collection
 
 app = Flask(__name__)
 
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    year = date.today().year
-    id_year = f'http://api.themoviedb.org/3/discover/movie?api_key=da396cb4a1c47c5b912fda20fd3a3336&primary_release_year={year}&sort_by=popularity.desc'
-    results = json.loads(requests.get(id_year).text)["results"]
-    for i in results:
-            if i["id"] and i["title"] and i["poster_path"] and i["vote_average"] and i["release_date"] and i["overview"] and i["backdrop_path"]:
-                results.append(movie(i["id"],i["title"],i["poster_path"],i["vote_average"],i["release_date"],i["overview"],i["backdrop_path"]))
-    return results
-
+    if request.method == 'GET':
+        year = date.today().year
+        id_year = f'http://api.themoviedb.org/3/discover/movie?api_key=da396cb4a1c47c5b912fda20fd3a3336&primary_release_year={year}&sort_by=popularity.desc'
+        top_year = movie_collection()
+        top_year.results = []
+        top_year.fetch(id_year)
+        genre = json.loads(requests.get('https://api.themoviedb.org/3/genre/movie/list?api_key=da396cb4a1c47c5b912fda20fd3a3336&language=en-US'))
+        top_genre_collection = []
+        for genre in genre:
+            genre_id = f'https://api.themoviedb.org/3/genre/movie/list?api_key=da396cb4a1c47c5b912fda20fd3a3336&&with_genre={genre["id"]}&sort_by=popularity.desc'
+            top_genre = movie_collection()
+            top_genre.results = []
+            top_genre.fetch(genre_id)
+            top_genre_id = [top_genre.results, genre['name']]
+            top_genre_collection.append(top_genre_id)
+    return render_template("home.html", top_year = top_year.results, year = year, top_genre = top_genre_collection)
 
 if __name__=="__main__":
     app.run(port=5000, debug=True)
