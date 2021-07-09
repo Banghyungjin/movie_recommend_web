f1188f126dc3bf6ef982588f341c88dd87cff6b7
HyungJin_Bang <37135305+Banghyungjin@users.noreply.github.com>
2021-07-09 11:43:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 import pandas as pd
 
 import requests
+from fetch import movie, movie_collection
 
 app = Flask(__name__)
 
@@ -13,9 +14,11 @@ app = Flask(__name__)
 def index():
     year = date.today().year
     id_year = f'http://api.themoviedb.org/3/discover/movie?api_key=da396cb4a1c47c5b912fda20fd3a3336&primary_release_year={year}&sort_by=popularity.desc'
-    top_year = json.loads(requests.get(id_year).text)
 
-    return top_year
+    top_year = movie_collection()
+    top_year.fetch(id_year)
+    
+    return top_year.results
 
 
 if __name__=="__main__":
